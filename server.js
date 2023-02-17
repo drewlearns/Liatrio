2b9ee5ef73b867c3f80149eed45ac68dd25bf541
drewlearns <drewlearns@hey.com>
2023-02-17 13:31:08 -0500

    fix port collision

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = new express();
-const port = 3000;
+const port = 3001;
 app.listen(port, ()=>{
     console.log(`
     Serving Application on port ${port}
