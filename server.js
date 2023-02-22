d60cf18cd459cf56db2d514077e8b2287fe08fe5
drewlearns <drewlearns@hey.com>
2023-02-22 12:09:33 -0500

    objective

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-const express = require('express');
+const express = require("express");
 const app = new express();
 const port = 80;
-app.listen(port, ()=>{
-    console.log(`
-    Serving Application on port ${port}
+app.listen(port, () => {
+  console.log(`
+    Serving Application on port http://localhost:${port}
     $$\\       $$\\             $$\\               $$\\           
     $$ |      \\__|            $$ |              \\__|          
     $$ |      $$\\  $$$$$$\\  $$$$$$\\    $$$$$$\\  $$\\  $$$$$$\\  
@@ -13,16 +13,14 @@ app.listen(port, ()=>{
     $$$$$$$$\\ $$ |\\$$$$$$$ |  \\$$$$  |$$ |      $$ |\\$$$$$$  |
     \\________|\\__| \\_______|   \\____/ \\__|      \\__| \\______/ 
     A simple API - By Drew Karriker
-    `)
-})
+    `);
+});
 
 app.get("/", (req, res) => {
-    console.log("New Request was made at", Date.now());
-    res.json(
-        {
-            "message": "Automate all the things!",
-            "timestamp": Date.now()
-        }
-                
-    )
-})
+  console.log("New Request was made at", Date.now());
+  res.json({
+    message: "Automate all the things!",
+    timestamp: Date.now(),
+    objective : "Hire Drew Karriker"
+  });
+});
